b7d3af551311cf353b0c52b18a6a8d230bcc00fc
Zach Weber <weberzt@gmail.com>
2020-02-04 14:48:36 -0600

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Artist.destroy_all
+Song.destroy_all
+
+tame_impala = Artist.create(name: "Tame Impala")
+fleet_foxes = Artist.create(name: "Fleet Foxes")
+
+endors_toi = Song.create(title: "Endors Toi", artist_id: 1)
+wwh = Song.create(title: "White Winter Hymnal", artist_id: 2)
